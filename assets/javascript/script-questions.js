1c8a610b7cebe4553dfd0089bbb1364bf31ab305
Norberto Mantohac <nmantohac82098@gmail.com>
2020-03-30 00:15:28 -0700

    completed code and css styling with added images, but no change in questions array

---
+++ assets/javascript/script-questions.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        title: "Which of these is NOT a handheld game console?",
+        choices: ["Game Boy Advance", "Nintendo GameCube", "Playstation Vita", "Nintendo 3DS", "Playstation Portable"],
+        answer: "Nintendo GameCube"
+    },
+    {
+        title: "Which of these characters IS a Nintendo mascot?",
+        choices: ["Sonic the Hedgehog", "Master Chief", "Crash Bandicoot", "Donkey Kong", "Mega Man"],
+        answer: "Donkey Kong"
+    },
+    {
+        title: "Which of these games is NOT listed as an FPS genre?",
+        choices: ["Call of Duty", "Quake", "Doom", "Diablo", "Counter-Strike"],
+        answer: "Diablo"
+    },
+    {
+        title: "Which of these is NOT from a fighting game series?",
+        choices: ["Final Fantasy", "Tekken", "Mortal Kombat", "Injustice", "Street Fighter"],
+        answer: "Final Fantasy",
+    },
+    {
+        title: "Which of these game series is NOT a Sony/Playstation exclusive title?",
+        choices: ["The Last of Us", "Horizon Zero Dawn", "Bayonetta", "inFAMOUS", "Ratchet & Clank"],
+        answer: "Bayonetta"
+    },
+    {
+        title: "Which of these popular indie game titles IS known for its unique enemies/bosses with distinctive personality traits?",
+        choices: ["INSIDE", "Super Meat Boy", "Braid", "Stardew Valley", "Undertale"],
+        answer: "Undertale"
+    },
+    {
+        title: "The Halo franchise is known to be one of the most iconic videogame franchises in history, and is developed/published under Microsoft. It is a well-known FPS series, but the titles 'Halo Wars & Halo Wars 2' are a completely different genre. What genre is it?",
+        choices: ["MMO", "Real-Time Strategy", "Action-Adventure", "RPG", "Stealth"],
+        answer: "Real-Time Strategy"
+    },
+    {
+        title: "JRPGs are a significant and notable type of RPG that are made in Japan, and are generally well-received in the videogame industry. Which of these JRPGs is a relatively new series that doesn't have a long history of titles?",
+        choices: ["Persona", "Ni no Kuni", "Xeno (Xenosaga/Xenoblade)", "Dragon Quest", "Tales"],
+        answer: "Ni no Kuni"
+    },
+    {
+        title: "FromSoftware is a videogame development company that are best known for their Soulsborne titles, which are a series of very challenging action-RPG games. Which one of these Soulsborne games have the objective of slaying the Lords of Cinder?",
+        choices: ["Dark Souls 2", "Demon's Souls", "Bloodborne", "Dark Souls", "Dark Souls 3"],
+        answer: "Dark Souls 3"
+    },
+    {
+        title: "Reboots/Remakes/Remastered Versions of past videogame titles or series has become a norm in the current era of gaming. Which of these games serves as a REMAKE of a past title that was first released in the PS2?",
+        choices: ["Shadow of the Colossus (2018)", "Prey (2017)", "Resident Evil 2 (2019)", "Killer Instinct (2013)", "Tomb Raider (2013)"],
+        answer: "Shadow of the Colossus (2018)"
+    },
+]
---
+++ assets/javascript/script.js
@@ -0,0 +1,256 @@
+const $startScreen = $("#start-screen");
+const $startScreenTitle = $("#quiz-title");
+const $startBtn = $("#start-button");
+const $timer = $("#timer")
+const $timeCount = $("#time");
+
+const $questionScreen = $("#question-screen");
+const $questionCard = $("#question-card");
+
+const $endScreen = $("#end-screen");
+const $endCard = $("#end-screen-card");
+
+const $highscoresBtn = $("#view-highscores-button");
+const $highscoresScreen = $("#highscores-screen");
+const $highscoresBody = $("#highscores-screen-body");
+const $goBackBtn = $("#go-back-button");
+const $clearBtn = $("#clear-button");
+
+ let totalTime = 120;
+ let quizTime = 0;
+
+ let correct = 0;
+ let wrong = 0;
+
+function quizGame() {
+
+    function initialize() {
+        $(document).ready(function() {
+            $startBtn.on("click", startQuiz);
+            $highscoresBtn.on("click", viewRankings);
+            $goBackBtn.on("click", goBackStart);
+            $clearBtn.on("click", clearScores);
+    });
+    };
+
+    function startQuiz() {
+        $startScreenTitle.hide();
+        $startScreen.hide();
+        $timer.show();
+        $questionScreen.show();
+
+        showQuestions();
+
+        $timeCount.text(totalTime);
+        timerInterval();
+    };
+
+    function showQuestions() {
+        const question = questions.shift();
+        const questionTitle = $(`<div class="card-header" id="question-title">
+                                    <h2>${question.title}</h2>
+                                </div>`)
+        
+        const choicesList = $(`<div class="card-body" id="question-choices>
+                               <ul class="list-group">
+                               </ul>
+                               </div>`);
+        
+        choicesList.on("click", (event) => {manageAnswerClick(event, question.answer)});
+        
+        question["choices"].forEach(choice => {
+            choicesList.append(`<button type="button" class="button-choices btn btn-warning">${choice}</button>
+                                <br>`);       
+        });
+
+        $questionCard.prepend(questionTitle);
+        $questionCard.append(choicesList);
+    };
+
+    function manageAnswerClick(event, answer) {
+        event.preventDefault();
+
+        if ($(event.target).html() === answer) {
+            event.target.style.backgroundColor = '#164032';
+            correct++;
+            setTimeout(function() {
+                $questionCard.empty();
+                if (questions.length !== 0) {
+                    showQuestions();
+                }
+                else {
+                    quizTime = totalTime;
+                    endQuiz();
+                    $timeCount.text(totalTime);
+                    totalTime = 0;
+                }
+            }, 500);
+        }
+        else {
+            event.target.style.borderColor = '#e53935';
+            event.target.style.backgroundColor = '#661917';
+            wrong++;
+            setTimeout(function() {
+            totalTime -= 20;
+            if (totalTime <= 0) {
+                totalTime = 0;
+            }
+            $questionCard.empty();
+            showQuestions();
+            }, 500);
+        }
+    };
+
+    function timerInterval() {
+        if (totalTime === 0) {
+            endQuiz();
+            $timeCount.text(totalTime);
+        }
+        else if (totalTime > 0) {
+            $timeCount.text(totalTime--);
+            setTimeout(timerInterval, 1000);
+        }
+    };
+
+    function endQuiz() {
+        showEndScore();
+        const $submitBtn = $("#end-screen-submit");
+        $submitBtn.on("click", manageInputSubmit);
+    };
+
+    function showEndScore() {
+        $questionScreen.hide();
+        $timer.hide();
+        $endScreen.show();
+        
+        var endScoreCard = $endCard.html(
+            `<div class="card-header" id="end-screen-header">
+                <h2 id="end-screen-title">Game Over!</h2>
+             </div>
+             <div class="card-body" id="end-screen-body">
+               <h3 id="final-score">Final Score: ${quizTime}</h3>
+               <p>Correct Answers: ${correct}</p>
+               <p>Wrong Answers: ${wrong}</p>
+               <p>Overall Percent: ${((correct / "10") * 100).toFixed(0)}%</p>  
+               Enter Username/Initials:
+               <div class="input-group mb-3">
+                 <input type="text" class="form-control player" id="end-screen-input" placeholder="Username/Initials" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                 <div class="input-group-append">
+                   <button class="btn btn-warning" id="end-screen-submit" type="button">Submit</button>
+                 </div>
+               </div>
+             </div>`);
+        
+        $endCard.prepend(endScoreCard);
+    }
+
+    function manageInputSubmit(event) {
+        event.preventDefault();
+
+        $endScreen.hide();
+        $highscoresBtn.show();
+        $highscoresScreen.show();
+        
+        const playerName = $(".player").val();
+        const player = {
+            name: playerName,
+            score: quizTime
+        }
+        
+        localStorageSave(player);
+        showRankings(player);        
+    };
+
+    function showRankings(currentPlayer = {}) {
+        const playersArr = JSON.parse(localStorage.getItem("playersArr"));
+        
+        const playerList = $('<ul class="list-group list-group-flush"></ul>');
+
+        if (playersArr !== null) {
+            sortArray(playersArr);
+            playersArr.forEach((player, index) => {
+                if (currentPlayer.name === player.name && currentPlayer !== {}) {
+                    playerList.append(`<li class="list-group-item font-weight-bold mt-1">${index + 1}. ${player.name} <span class="player-score">${player.score}</span></li>`);
+                }
+                else {
+                    playerList.append(`<li class="list-group-item mt-1">${index + 1}. ${player.name} <span class="player-score">${player.score}</span></li>`);
+                }
+            });
+        }
+
+        $highscoresBody.prepend(playerList);
+
+    };
+
+    function viewRankings() {
+            $startScreenTitle.hide();
+            $startScreen.hide();
+            $questionScreen.hide()
+            $endScreen.hide();
+            $timer.hide();
+            $highscoresBody.empty();
+            $highscoresScreen.show();
+            showRankings();
+    };
+
+    function sortArray(arr) {
+        arr.sort((a,b) => {
+            const scoreA = a.score;
+            const scoreB = b.score;
+            if (scoreA < scoreB) {
+                return 1
+            }
+            else if (scoreA > scoreB) {
+                return -1
+            }
+            else {
+                return 0
+            }
+        });
+    };
+
+    function localStorageSave(player) {
+        if (localStorage.getItem("playersArr") === null) {
+            const playersArr = [];
+            playersArr.push(player);
+            localStorage.setItem("playersArr", JSON.stringify(playersArr));
+        }
+        else {
+            const playersArr = JSON.parse(localStorage.getItem("playersArr"));
+            playersArr.push(player);
+            localStorage.setItem("playersArr", JSON.stringify(playersArr));
+        }
+    };
+
+    function goBackStart() {
+        window.location.href = "./index.html";
+    };
+
+    function clearScores() {
+        localStorage.clear();
+        $highscoresBody.empty();
+    };
+    
+    initialize();
+};
+
+quizGame();
+
+const imagesArr = [
+    "./assets/images/Background-YQY_1.png",
+    "./assets/images/Background-MX_1.png",
+    "./assets/images/Background-YQY_2.png",
+    "./assets/images/Background-MX_2.png",
+];
+
+const $wrapper = $(".wrapper")
+let nextImage = 0;
+doSlideShow();
+
+function doSlideShow() {
+    if (nextImage >= imagesArr.length) {
+        nextImage = 0;
+    }
+    $wrapper.css("background-image", 'url("'+imagesArr[nextImage++]+'")')
+    setTimeout(doSlideShow, 8000);
+};
